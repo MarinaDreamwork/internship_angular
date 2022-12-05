ab4260e997bf273f545c60c0bc3262cf35648f07
Marina Trushina <silaevamo@gmail.com>
2022-12-05 19:15:59 +0300

    created routing for the existed pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { DescriptionProjectComponent } from './description-project/description-project.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'todo_list', component: TodoListComponent },
+  { path: 'auth', component: AuthComponent },
+  { path: 'about', component: DescriptionProjectComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
